fedf2717cf2d6b4ace95d305fd2f9e68ecf64dbc
Maksim <felozan4ik2007@gmail.com>
2025-08-19 01:26:02 +0200

    main page start

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Thread
+
 
 # Create your views here.
+def thread_list(request):
+    threads = Thread.objects.all()
+    return render(request, "forum/main.html", context={"threads": threads})
