7ade8792b6a551fa6f0a5ba1813f8b56a6e18b77
Maksim <felozan4ik2007@gmail.com>
2025-08-27 16:41:37 +0200

    managing threads

--- forum/views.py
+++ forum/views.py
@@ -12,7 +12,11 @@ def thread_detail(request, thread_id):
     thread = get_object_or_404(Thread, id=thread_id)
 
     if request.method == "GET":
-        return render(request, "forum/thread.html", context={"thread": thread})
+        context = {
+            "thread": thread,
+            "can_manage": can_manage_thread(request.user, thread),
+        }
+        return render(request, "forum/thread.html", context=context)
     elif request.method == "POST":
         text = request.POST.get("content")
         if text:
@@ -46,3 +50,42 @@ def create_thread(request):
             is_first=True,
         )
         return redirect("forum:thread", thread_id=thread.id)
+
+
+def can_manage_thread(user, thread):
+    if not user.is_authenticated:
+        return False
+    if user.profile.role in ["admin", "manager"]:
+        return True
+
+    first_post = thread.posts.filter(is_first=True).first()
+    if first_post and first_post.author == user:
+        return True
+
+
+def update_thread(request, thread_id):
+    thread = get_object_or_404(Thread, id=thread_id)
+
+    if not can_manage_thread(request.user, thread):
+        return redirect("forum:thread", thread_id=thread.id)
+
+    if request.method == "POST":
+        new_title = request.POST.get("title")
+        if new_title:
+            thread.title = new_title
+            thread.save()
+
+    return redirect("forum:thread", thread_id=thread.id)
+
+
+def delete_thread(request, thread_id):
+    thread = get_object_or_404(Thread, id=thread_id)
+
+    if not can_manage_thread(request.user, thread):
+        return redirect("forum:thread", thread_id=thread.id)
+
+    if request.method == "POST":
+        thread.delete()
+        return redirect("forum:main")
+
+    return redirect("forum:thread", thread_id=thread.id)
