ff3a08af2aa666510194cb25d9c92e07c2dc19cc
Vlad <vladkarpovec44@gmail.com>
2025-09-03 17:21:35 +0300

    fixing errors with "register" and logging out of the account

--- auth_system/views.py
+++ auth_system/views.py
@@ -13,7 +13,7 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("login")
+            return redirect("register:login")
     else:
         form = CustomUserCreationForm()
     return render(request, "registration/register.html", {"form": form})
@@ -24,7 +24,7 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             auth_login(request, user)
-            return redirect("profile")
+            return redirect("register:profile")
         else:
             messages.error(request, "Невірний логін або пароль")
     else:
@@ -42,7 +42,7 @@ def profile_view(request):
         if form.is_valid():
             form.save()
             messages.success(request, "✅ Профіль оновлено")
-            return redirect("profile")
+            return redirect("register:profile")
     else:
         form = ProfileUpdateForm(instance=profile)
 
@@ -51,4 +51,4 @@ def profile_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect("login")
+    return redirect("register:login")
